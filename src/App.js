c5916438e818de6a825e610d8879e0558e8100be
FernandaRonGf <fernanda@gafa.mx>
2019-12-11 19:50:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      message:'',
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      message:'Hellooooo'
+    })
+  }
+
+
+
+  render (){
+    return (
+       <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <h2>{this.state.message}</h2>
       </header>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
